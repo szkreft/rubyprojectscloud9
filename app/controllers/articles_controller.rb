b9fa42fb6b53de0e8b7dd5e812c5940b81700ca5
Sebastian Fiut <sebastianklf@gmail.com>
2016-11-19 17:49:02 +0000

    Edit and update action created

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,6 +5,10 @@ class ArticlesController < ApplicationController
     def new
         @article = Article.new
     end
+    def edit
+        @article = Article.find(params[:id])
+    end
+    
     def create
         @article = Article.new(article_params)
         if @article.save
@@ -14,7 +18,15 @@ class ArticlesController < ApplicationController
             render :new
         end
     end
-    
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(article_params)
+            flash[:notice] = "Article was successfully updated"
+            redirect_to article_path(@article)
+        else
+            render :edit
+        end
+    end
     def show
         @article = Article.find(params[:id])
     end
