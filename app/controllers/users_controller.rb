72c49762fd3fa155c3ccd6f08282485c842ff42d
Sebastian Fiut <sebastianklf@gmail.com>
2016-11-21 16:28:35 +0000

    add edit and update-partials

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,13 +6,30 @@ class UsersController < ApplicationController
    def create
       @user = User.new(user_params) 
       if @user.save
-          flash[:success] = "Welcome to the Sebastian's Blog #[@users.username]"
+          flash[:success] = "Welcome to the Sebastian's Blog #{@user.username}"
           redirect_to articles_path
       else
           render 'new'
       end
       
    end
+    
+   def edit
+       @user = User.find(params[:id])
+   end
+   
+   def update
+       @user = User.find(params[:id])
+       if @user.update(user_params)
+           flash[:success] = "Your account has been updated"
+           redirect_to articles_path
+           
+       else
+           render 'edit'
+       end
+       
+   end
+   
    
    private
    def user_params
