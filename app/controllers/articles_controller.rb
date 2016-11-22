31eb67f2386ef2ba9143589c18c20eea994c7cd9
Sebastian Fiut <sebastianklf@gmail.com>
2016-11-22 20:06:58 +0000

    admin functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,7 +17,7 @@ class ArticlesController < ApplicationController
     
     def create
         @article = Article.new(article_params)
-        @article.user = User.last
+        @article.user = current_user
         if @article.save
             flash[:success] = "Article was successfully created"
             redirect_to article_path(@article)
@@ -55,7 +55,7 @@ class ArticlesController < ApplicationController
      end
      
     def require_same_user
-        if current_user != @user
+        if current_user != @article.user and !current_user.admin?
             flash[:danger] = "You can only edit or delete your own articles!"
             redirect_to root_path
         end
