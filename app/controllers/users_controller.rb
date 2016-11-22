0c77d9334ea9a7cf71a081e4ad186158ac34a4fc
Sebastian Fiut <sebastianklf@gmail.com>
2016-11-22 19:19:58 +0000

    add sign in upon signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,8 +15,9 @@ class UsersController < ApplicationController
    def create
       @user = User.new(user_params) 
       if @user.save
+          session[:user_id] = @user.id
           flash[:success] = "Welcome to the Sebastian's Blog #{@user.username}"
-          redirect_to articles_path
+          redirect_to user_path(@user)
       else
           render 'new'
       end
@@ -47,7 +48,7 @@ class UsersController < ApplicationController
    def user_params
        params.require(:user).permit(:username, :email, :password)
    end
-   #set user method
+   #set user methodsss
    def set_user
       @user = User.find(params[:id])
    end
